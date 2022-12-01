7751c156637925905cae05bad25ce5bf22cdd346
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-01 00:26:38 -0600

    prepare for day 2

---
+++ src/aoc2022/Day02.kt
@@ -0,0 +1,24 @@
+package aoc2022
+
+import readInput
+
+fun main() {
+
+    fun part1() {
+
+    }
+
+    fun part2() {
+
+    }
+
+    val input = readInput("Day02", "2022")
+    println(part1())
+
+    println(part2())
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day012test", "2022")
+//    check(part1())
+//    check(part2())
+}

e8fcbde187427f9bd524c91dc48a9001a3f7de2e
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-01 00:24:08 -0600

    day 1 2022

---
+++ src/aoc2021/Day01.kt
@@ -0,0 +1,28 @@
+package aoc2021
+
+import readInput
+
+fun main() {
+
+    fun convertToInt(stringList: List<String>) : List<Int> {
+        return stringList.map { it.toInt() }
+    }
+
+    fun part1(input: List<String>): Int {
+        return convertToInt(input).zipWithNext().count { (a, b) -> a < b }
+    }
+
+    fun part2(input: List<String>): List<Int> {
+        return input.map { it.toInt() }.windowed(3).map { it.sum() }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_test", "2021")
+    check(part1(testInput) == 7)
+    check(part1(part2(testInput).map { it.toString() }) == 5)
+
+    val input = readInput("Day01", "2021")
+    println(part1(input))
+    println(part1(part2(input).map { it.toString() }))
+}
+
---
+++ src/aoc2022/Day01.kt
@@ -0,0 +1,37 @@
+package aoc2022
+
+import readInput
+
+fun main() {
+
+    fun part1(input: List<String>) : MutableList<Int> {
+        val sums = mutableListOf<Int>();
+        var sum = 0
+
+        for(num in input) {
+            if (num.isNotEmpty()){
+                sum += num.toInt()
+            } else {
+                sums.add(sum)
+                sum = 0
+                continue
+            }
+        }
+        return sums
+    }
+
+    fun part2(sums : MutableList<Int>) : Int {
+        sums.sort()
+        sums.reverse()
+        return sums.subList(0,3).sum()
+    }
+
+    val input = readInput("Day01", "2022")
+    println(part1(input).maxOrNull())
+    println(part2(part1(input)))
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day01_test", "2022")
+    check(part1(testInput).maxOrNull() == 100)
+    check(part2(part1(testInput)) == 145)
+}

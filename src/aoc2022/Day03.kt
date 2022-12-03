42cfa271e01fab05fa0cae796d6e3a1d00fee381
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-03 00:19:21 -0600

    Day 3 Solution

--- src/aoc2022/Day03.kt
+++ src/aoc2022/Day03.kt
@@ -1,23 +1,50 @@
 package aoc2022
 
 import readInput
+import alphaToInt
 
 fun main() {
 
-    fun part1() {
+    fun part1(input : List<String>) : Int {
+        var sum = 0
 
+        for(rucksack in input) {
+            val first = rucksack.substring(0, rucksack.length / 2)
+            val second = rucksack.substring(rucksack.length / 2)
+            //just initializes character as a non-empty value
+            var commonChar = 'a'
+            for(char in first){
+                if(second.contains(char)){
+                    commonChar = char
+                }
+            }
+
+            sum += alphaToInt(commonChar.toString())!!
+        }
+        return sum
     }
 
-    fun part2() {
-
+    fun part2(input : List<String>) : Int {
+        var sum = 0
+        for(group in input.windowed(3,3)){
+            //just initializes character as a non-empty value
+            var commonChar = 'a'
+            for(char in group[0]){
+                if(group[1].contains(char) && group[2].contains(char)){
+                    commonChar = char
+                }
+            }
+            sum += alphaToInt(commonChar.toString())!!
+        }
+        return sum
     }
 
     val input = readInput("Day03", "2022")
-    part1()
-    part2()
+    println(part1(input))
+    println(part2(input))
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test", "2022")
-    check(part1())
-    check(part2())
+    val testInput = readInput("Day03_test", "2022")
+    check(part1(testInput) == 77)
+    check(part2(testInput) == 16)
 }

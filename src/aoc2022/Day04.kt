ee8578e1b3d71209ba056b1c7ea17674572ee694
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-03 23:34:34 -0600

    Day 4 Solution

--- src/aoc2022/Day04.kt
+++ src/aoc2022/Day04.kt
@@ -4,14 +4,42 @@ import utils.readInput
 
 fun main() {
 
-    fun part1(input: List<String>) : List<Int> {
-        return listOf()
+    fun part1(input: List<String>) : Int  {
+        var fullContains = 0
+
+        for(assignment in input) {
+            val (elf1, elf2) = assignment.split(",")
+            val (min1, max1) = elf1.split("-").map(String::toInt)
+            val (min2, max2) = elf2.split("-").map(String::toInt)
+
+            if(
+                min1 <= min2 && max1 >= max2 || min2 <= min1 && max2 >= max1
+            ) fullContains++
+        }
+
+        return fullContains
+
     }
 
-    fun part2(sums : List<String>) : Int {
+    fun part2(input : List<String>) : Int {
+        var allContains = 0
 
-        return 0
+        for(assignment in input) {
+            val (elf1, elf2) = assignment.split(",")
+            val (min1, max1) = elf1.split("-").map(String::toInt)
+            val (min2, max2) = elf2.split("-").map(String::toInt)
+
+            val pair1 = min1..max1
+            val pair2 = min2..max2
+
+            if(
+                pair1.first in pair2 || pair1.last in pair2 || pair2.first in pair1 || pair2.last in pair1
+            ) allContains++
+        }
+
+        return allContains
     }
+
 
     val input = readInput("Day04", "2022")
     println(part1(input))
@@ -19,6 +47,6 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test", "2022")
-//    check()
-//    check()
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 }

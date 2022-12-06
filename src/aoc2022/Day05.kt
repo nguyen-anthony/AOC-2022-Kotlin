52a9eb222a6e5fd9078da7918145238693b8e17a
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-05 18:48:36 -0600

    Day 5 Solution - minor changes to remove hard coding stacks

--- src/aoc2022/Day05.kt
+++ src/aoc2022/Day05.kt
@@ -5,21 +5,20 @@ import utils.readInput
 fun main() {
 
     fun part1(input: List<String>) : String {
-        //manual create of each stack
-        val stack1 = ArrayDeque(mutableListOf("R","G","J","B","T","V","T"))
-        val stack2 = ArrayDeque(mutableListOf<String>("J","R","V","L"))
-        val stack3 = ArrayDeque(mutableListOf<String>("S","Q","F"))
-        val stack4 = ArrayDeque(mutableListOf<String>("Z","H","N","L","F","V","Q","G"))
-        val stack5 = ArrayDeque(mutableListOf<String>("R","Q","T","J","C","S","M","W"))
-        val stack6 = ArrayDeque(mutableListOf<String>("S","W","T","C","H","F"))
-        val stack7 = ArrayDeque(mutableListOf<String>("D","Z","C","V","F","N","J"))
-        val stack8 = ArrayDeque(mutableListOf<String>("L","G","Z","D","W","R","F","Q"))
-        val stack9 = ArrayDeque(mutableListOf<String>("J","B","W","V","P"))
-        val allStacks = mutableListOf(stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9)
+        val split = input.indexOf("")
+        val stacks = input.slice(0 until split)
+        val instructionsList = input.slice(split+1 until input.size)
+        val allStacks = mutableListOf<ArrayDeque<String>>()
+        for(stack in stacks) {
+            val crates = stack.chunked(1)
+            allStacks.add(ArrayDeque(crates))
+        }
 
-        for(line in input) {
-            //removed "move", "from", and "to" so each line is just three numbers
-            var (numMoves, fromStack, toStack) = line.split(" ").map(String::toInt)
+        for(line in instructionsList) {
+            val instructions = line.split(" ")
+            var numMoves = instructions[1].toInt()
+            val fromStack = instructions[3].toInt()
+            val toStack = instructions[5].toInt()
             while(numMoves > 0){
                 allStacks[toStack-1].add(allStacks[fromStack-1].removeLast())
                 numMoves--
@@ -36,19 +35,20 @@ fun main() {
     }
 
     fun part2(input : List<String>) : String {
-        val stack1 = ArrayDeque(mutableListOf("R","G","J","B","T","V","T"))
-        val stack2 = ArrayDeque(mutableListOf<String>("J","R","V","L"))
-        val stack3 = ArrayDeque(mutableListOf<String>("S","Q","F"))
-        val stack4 = ArrayDeque(mutableListOf<String>("Z","H","N","L","F","V","Q","G"))
-        val stack5 = ArrayDeque(mutableListOf<String>("R","Q","T","J","C","S","M","W"))
-        val stack6 = ArrayDeque(mutableListOf<String>("S","W","T","C","H","F"))
-        val stack7 = ArrayDeque(mutableListOf<String>("D","Z","C","V","F","N","J"))
-        val stack8 = ArrayDeque(mutableListOf<String>("L","G","Z","D","W","R","F","Q"))
-        val stack9 = ArrayDeque(mutableListOf<String>("J","B","W","V","P"))
-        val allStacks = mutableListOf(stack1, stack2, stack3, stack4, stack5, stack6, stack7, stack8, stack9)
+        val split = input.indexOf("")
+        val stacks = input.slice(0 until split)
+        val instructionsList = input.slice(split+1 until input.size)
+        val allStacks = mutableListOf<ArrayDeque<String>>()
+        for(stack in stacks) {
+            val crates = stack.chunked(1)
+            allStacks.add(ArrayDeque(crates))
+        }
 
-        for(line in input) {
-            var (numBoxes, fromStack, toStack) = line.split(" ").map(String::toInt)
+        for(line in instructionsList) {
+            val instructions = line.split(" ")
+            var numBoxes = instructions[1].toInt()
+            val fromStack = instructions[3].toInt()
+            val toStack = instructions[5].toInt()
             val fromStackList = allStacks[fromStack-1]
             val cratesToMove = fromStackList.slice((fromStackList.size - numBoxes) until fromStackList.size)
 
@@ -76,6 +76,6 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test", "2022")
-//    check(part1(testInput) == 2)
-//    check(part2(testInput) == 4)
+    check(part1(testInput) == "CMZ")
+    check(part2(testInput) == "MCD")
 }

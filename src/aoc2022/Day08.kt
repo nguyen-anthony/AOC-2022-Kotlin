bdec71bc6c1a69f27ea78492e912d8788d0afc21
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-07 23:27:19 -0600

    Added 2D array reading

--- src/aoc2022/Day08.kt
+++ src/aoc2022/Day08.kt
@@ -1,9 +1,16 @@
 package aoc2022
 
 import utils.readInputAsList
+import utils.readInputAsMatrix
 
 fun main() {
-    fun part1(input: List<String>) : Any {
+    fun part1(input: Array<Array<String>>) : Any {
+        for(i in 0 until input.size) {
+            for(j in 0 until input[i].size){
+                print(input[i][j] + "")
+            }
+            println()
+        }
         return 0
     }
 
@@ -13,11 +20,11 @@ fun main() {
 
 
 
-    val input = readInputAsList("Day08", "2022")
+    val input = readInputAsMatrix("Day08", "2022")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 
     val testInput = readInputAsList("Day08_test", "2022")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+//    check(part1(testInput) == 21)
+//    check(part2(testInput) == 0)
 }
--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -1,6 +1,8 @@
 package utils
 
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileReader
 import java.math.BigInteger
 import java.security.MessageDigest
 
@@ -13,6 +15,23 @@ val alphaToIntMap = ('a'..'z').zip(1..26).plus(('A'..'Z').zip(27..52)).associate
 fun readInputAsList(name: String, year: String) = File("src/aoc$year/resources", "$name.txt").readLines()
 
 fun readInputAsString(name: String, year: String) = File("src/aoc$year/resources", "$name.txt").readText()
+
+fun readInputAsMatrix(name: String, year: String) : Array<Array<String>>{
+    val file = File("src/aoc$year/resources", "$name.txt")
+    val reader = BufferedReader(file.reader())
+
+    val line = reader.readLine()
+    val words = line.split("")
+    val twoDArray = Array(file.length().toInt()) { Array(words.size) { "" } }
+    for( i in 0 until file.length().toInt()) {
+        for(j in 0 until words.size) {
+            twoDArray[i][j] = words[j]
+        }
+    }
+    reader.close()
+    return twoDArray
+}
+
 /**
  * Converts string to utils.md5 hash.
  */

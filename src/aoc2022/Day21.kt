6d16baa5223f64fac2a48df2c1fee7aab2893373
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-07 22:14:05 -0600

    Advent of Code days file prep

---
+++ src/aoc2022/Day21.kt
@@ -0,0 +1,23 @@
+package aoc2022
+
+import utils.readInputAsList
+
+fun main() {
+    fun part1(input: List<String>) : Any {
+        return 0
+    }
+
+    fun part2(input: List<String>) : Any {
+        return 0
+    }
+
+
+
+    val input = readInputAsList("Day21", "2022")
+    println(part1(input))
+    println(part2(input))
+
+    val testInput = readInputAsList("Day21_test", "2022")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+}

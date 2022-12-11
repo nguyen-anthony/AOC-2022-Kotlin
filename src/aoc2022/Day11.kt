cec6c279fb245a11bb2bdf067d96724852e28302
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-11 00:23:26 -0600

    Day 11 progress

--- src/aoc2022/Day11.kt
+++ src/aoc2022/Day11.kt
@@ -2,8 +2,33 @@ package aoc2022
 
 import utils.readInputAsList
 
+data class Monkey(
+    val items: MutableList<Int>,
+    val op: String,
+    val opValue: Int?,
+    val test: Int,
+    val ifTrue: Int,
+    val ifFalse: Int,
+    var inspections: Int = 0
+)
+
 fun main() {
+
+    fun getInputs(input: List<String>): List<Monkey> {
+        return input.windowed(7, 7).map { monkey ->
+            Monkey(
+                monkey[1].drop("  Starting items: ".length).split(", ").map { it.toInt() }.toMutableList(),
+                if(monkey[2].contains("*")) "multiply" else "add",
+                monkey[2].split(" ").last().toIntOrNull(),
+                monkey[3].split(" ").last().toInt(),
+                monkey[4].split(" ").last().toInt(),
+                monkey[5].split(" ").last().toInt(),
+            )
+        }
+    }
+
     fun part1(input: List<String>) : Any {
+        println(getInputs(input))
         return 0
     }
 

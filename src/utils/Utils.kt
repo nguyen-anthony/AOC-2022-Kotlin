0788d232486b02944780d9104f82a499f87c0a2f
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-11 23:49:39 -0600

    Fix input as matrix

--- src/utils/Utils.kt
+++ src/utils/Utils.kt
@@ -24,12 +24,27 @@ fun readInputAsMatrix(name: String, year: String) : Array<Array<String>>{
     val words = line.split("")
     val twoDArray = Array(file.length().toInt()) { Array(words.size) { "" } }
     for( i in 0 until file.length().toInt()) {
-        for(j in 0 until words.size) {
+        for(j in words.indices) {
             twoDArray[i][j] = words[j]
         }
     }
     reader.close()
     return twoDArray
+}
+
+fun readFileTo2DArray(name: String, year: String): Array<Array<String>> {
+    // Open the file using the File class
+    val file = File("src/aoc$year/resources", "$name.txt")
+    val reader = BufferedReader(file.bufferedReader())
+
+    // Read the file line by line and store each line as an element in a list of strings
+    val lines: List<String> = reader.readLines()
+
+    // Split each string on the delimiter and convert it to a list of strings
+    val splitLines = lines.map { it.split("") }
+
+    // Convert each element in the inner lists to an Int and return the resulting 2D array
+    return splitLines.map { it.toTypedArray() }.toTypedArray()
 }
 
 /**

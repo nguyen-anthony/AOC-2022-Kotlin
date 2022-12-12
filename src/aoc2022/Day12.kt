2967f011ed0c056d90fdf501a50c79acf38e5895
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-11 23:49:53 -0600

    Day 12 Progress

--- src/aoc2022/Day12.kt
+++ src/aoc2022/Day12.kt
@@ -1,9 +1,37 @@
 package aoc2022
 
+import utils.readFileTo2DArray
 import utils.readInputAsList
+import utils.readInputAsMatrix
 
 fun main() {
-    fun part1(input: List<String>) : Any {
+
+    fun getStartAndEnd(input: Array<Array<String>>) : Pair<Any, Any> {
+        lateinit var start : Pair<Int, Int>
+        lateinit var end : Pair<Int, Int>
+        // Iterate over the rows of the array
+        for (i in input.indices) {
+            // Iterate over the columns of the current row
+            for (j in input[i].indices) {
+                if(input[i][j] == "S"){
+                    start = Pair(i, j)
+                }
+                if(input[i][j] == "E"){
+                    end = Pair(i, j)
+                }
+            }
+
+        }
+        return Pair(start, end)
+    }
+
+    fun part1(input: Array<Array<String>>) : Any {
+        var (start, end) = getStartAndEnd(input)
+
+//        println(start)
+//        println(end)
+//        println(input[start.first][start.second])
+//        println(input[end.first][end.second])
         return 0
     }
 
@@ -13,11 +41,11 @@ fun main() {
 
 
 
-    val input = readInputAsList("Day12", "2022")
+    val input = readFileTo2DArray("Day12", "2022")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 
-    val testInput = readInputAsList("Day12_test", "2022")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+//    val testInput = readInputAsList("Day12_test", "2022")
+//    check(part1(testInput) == 0)
+//    check(part2(testInput) == 0)
 }

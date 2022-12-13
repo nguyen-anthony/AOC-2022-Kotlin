be77dfc88f1290da76cfbd26d0da212f218144e2
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2022-12-12 22:33:21 -0600

    Day 12 Solution

--- src/aoc2022/Day12.kt
+++ src/aoc2022/Day12.kt
@@ -1,49 +1,73 @@
 package aoc2022
 
-import utils.readFileTo2DArray
 import utils.readInputAsList
-import utils.readInputAsMatrix
-
+data class Step(val point: Pair<Int,Int>, val distance: Int, val parent: Step?)
 fun main() {
 
-    fun getStartAndEnd(input: Array<Array<String>>) : Pair<Any, Any> {
-        lateinit var start : Pair<Int, Int>
-        lateinit var end : Pair<Int, Int>
-        // Iterate over the rows of the array
-        for (i in input.indices) {
-            // Iterate over the columns of the current row
-            for (j in input[i].indices) {
-                if(input[i][j] == "S"){
-                    start = Pair(i, j)
+    fun letter(ch: Char): Char {
+        return when (ch) {
+            'S' -> 'a'
+            'E' -> 'z'
+            else -> ch
+        }
+    }
+
+    fun findShortest(start: Pair<Int,Int>, map: List<List<Char>>): Int {
+        val queue = ArrayDeque<Step>()
+        val visited = mutableSetOf(start)
+        queue.addFirst(Step(start, 0, null))
+        while (queue.isNotEmpty()) {
+            val node = queue.removeFirst()
+            if (map[node.point.first][node.point.second] == 'E') {
+                return node.distance
+            }
+            setOf(
+                if (node.point.second < map[node.point.first].size - 1) node.point.first to node.point.second + 1 else null,
+                if (node.point.second > 0) node.point.first to node.point.second - 1 else null,
+                if (node.point.first > 0) node.point.first - 1 to node.point.second else null,
+                if (node.point.first < map.size - 1) node.point.first + 1 to node.point.second else null)
+                .filterNotNull()
+                .filter {letter(map[it.first][it.second]) - letter(map[node.point.first][node.point.second]) <= 1 }
+                .filter { !visited.contains(it) }
+                .forEach {
+                    visited.add(it)
+                    queue.add(Step(it, node.distance + 1, node))
                 }
-                if(input[i][j] == "E"){
-                    end = Pair(i, j)
+        }
+        return Int.MAX_VALUE
+    }
+
+    fun part1(input: List<String>): Int {
+        val map = input.map{ it.toList() }
+        lateinit var starts : Pair<Int,Int>
+        for(i in map.indices) {
+            for (j in 0 until map[i].size) {
+                if (map[i][j] == 'S') {
+                    starts = i to j
                 }
             }
-
         }
-        return Pair(start, end)
+
+        return findShortest(starts, map)
     }
 
-    fun part1(input: Array<Array<String>>) : Any {
-        var (start, end) = getStartAndEnd(input)
+    fun part2(input: List<String>): Int {
+        val map = input.map{ it.toList() }
+        var starts = mutableListOf<Pair<Int,Int>>()
 
-//        println(start)
-//        println(end)
-//        println(input[start.first][start.second])
-//        println(input[end.first][end.second])
-        return 0
+        for(i in map.indices) {
+            for (j in 0 until map[i].size) {
+                if (map[i][j] == 'S' || map[i][j] == 'a') {
+                    starts.add(i to j)
+                }
+            }
+        }
+        return starts.minOf { findShortest(it, map) }
     }
 
-    fun part2(input: List<String>) : Any {
-        return 0
-    }
-
-
-
-    val input = readFileTo2DArray("Day12", "2022")
+    val input = readInputAsList("Day12", "2022")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 
 //    val testInput = readInputAsList("Day12_test", "2022")
 //    check(part1(testInput) == 0)
